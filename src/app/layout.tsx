766b622f190a9bc9d3dc8f21c1b7480b263abca1
seanaxn1135 <seanaxn1135@gmail.com>
2023-06-17 22:06:19 +0800

    style: add montserrat font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
 import './globals.css'
 
 import Header from '@/components/layout/header'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Showerthoughts',
@@ -14,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={montserrat.className}>
       <body>
         <Header />
         <main>{children}</main>
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,14 +5,16 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <header className="container mx-auto flex items-center border-b-2 px-6 py-2 h-24">
-      <Image
-        src="/showerthoughts_logo.png"
-        alt="Shower Thoughts Logo"
-        width={100}
-        height={100}
-      />
-      <h1 className="font-bold">Showerthoughts</h1>
+    <header className="container mx-auto flex items-center px-6 py-2 h-24">
+      <div className="flex items-center">
+        <Image
+          src="/showerthoughts_logo.png"
+          alt="Shower Thoughts Logo"
+          width={100}
+          height={100}
+        />
+        <h1 className="font-bold">Showerthoughts</h1>
+      </div>
       <div className="flex-grow"></div>
       <div>
         <Navbar />
