f7bcad16e57d320dad5c3197ae7546f56ffa08a1
seanaxn1135 <seanaxn1135@gmail.com>
2023-06-17 21:10:53 +0800

    style: add images for styling of website

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,17 @@
 'use client'
 
 import Navbar from '../ui/navbar'
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <header className="container mx-auto flex items-center border-b-2 px-6 py-2 h-24">
+      <Image
+        src="/showerthoughts_logo.png"
+        alt="Shower Thoughts Logo"
+        width={100}
+        height={100}
+      />
       <h1 className="font-bold">Showerthoughts</h1>
       <div className="flex-grow"></div>
       <div>
