40979e3940933b692095a5b4d16bb574fefedd8c
seanaxn1135 <seanaxn1135@gmail.com>
2023-06-25 16:55:54 +0800

    style: tweak button padding for sizes below sm

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ export default function Button({
     <>
       <Link
         href={href}
-        className={`lg:text-lg md:text-md sm:text-sm flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-xs font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark ${className}`}
+        className={`lg:text-lg md:text-md sm:text-sm sm:px-6 flex items-center bg-dark text-light p-2.5 px-4 rounded-lg text-xs font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark ${className}`}
       >
         {children}
       </Link>
