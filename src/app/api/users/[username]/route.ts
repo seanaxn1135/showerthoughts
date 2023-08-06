19ccb5af0f9c7f1432f33f982521580700706960
seanaxn1135 <seanaxn1135@gmail.com>
2023-08-06 22:19:57 +0800

    api: add api for finding user by username

---
+++ src/app/api/users/[username]/route.ts
@@ -0,0 +1,21 @@
+import Users from '@/app/models/Users'
+import connectMongo from '@/app/utils/dbConfig'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { username: string } }
+) {
+  try {
+    await connectMongo()
+
+    const username = params.username
+    const user = await Users.findOne({ username: username })
+    return NextResponse.json(user, { status: 200 })
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Failed to fetch user ' + params.username },
+      { status: 500 }
+    )
+  }
+}
