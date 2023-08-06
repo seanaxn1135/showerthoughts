1762d0ededed3f735bb603645894cce0dd941784
seanaxn1135 <seanaxn1135@gmail.com>
2023-08-06 21:32:00 +0800

    api: moved post and create get api for users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,6 +3,19 @@ import Users from '@/app/models/Users'
 import connectMongo from '@/app/utils/dbConfig'
 import { NextRequest, NextResponse } from 'next/server'
 
+export async function GET() {
+  try {
+    await connectMongo()
+    const users = await Users.find()
+    return NextResponse.json(users, { status: 200 })
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Failed to fetch users' },
+      { status: 500 }
+    )
+  }
+}
+
 export async function POST(req: NextRequest) {
   try {
     const reqBody = await req.json()
