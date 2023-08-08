fc6793909e8417e559fff089dd00ab429d764083
seanaxn1135 <seanaxn1135@gmail.com>
2023-08-08 15:30:11 +0800

    fix: revert last commit

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,5 @@
 import type { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
-import { NextResponse } from 'next/server'
 
 export const options: NextAuthOptions = {
   providers: [
@@ -19,32 +18,24 @@ export const options: NextAuthOptions = {
         },
       },
       async authorize(credentials) {
-        try {
-          const BASE_URL = process.env.BASE_URL
-          const LOGIN_API_URL = '/api/login'
-          const res = await fetch(`${BASE_URL}${LOGIN_API_URL}`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              username: credentials?.username,
-              password: credentials?.password,
-            }),
-          })
+        const BASE_URL = process.env.BASE_URL
+        const LOGIN_API_URL = '/api/login'
+        const res = await fetch(`${BASE_URL}${LOGIN_API_URL}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username: credentials?.username,
+            password: credentials?.password,
+          }),
+        })
+        const user = await res.json()
 
-          const user = await res.json()
-
-          if (user) {
-            return user
-          } else {
-            return null
-          }
-        } catch (error) {
-          return NextResponse.json(
-            { error: 'Internal server error' },
-            { status: 500 }
-          )
+        if (user) {
+          return user
+        } else {
+          return null
         }
       },
     }),
