b236208d7aeb40e3b5fde3c748480bf4689be391
seanaxn1135 <seanaxn1135@gmail.com>
2023-08-09 10:43:11 +0800

    ci: change deploy to prod to preview

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export const server =
-  process.env.VERCEL_ENV === 'production'
+  process.env.VERCEL_ENV === 'production' ||
+  process.env.VERCEL_ENV === 'preview'
     ? 'https://' + process.env.VERCEL_URL
-    : 'http://localhost:3000'
+    : process.env.BASE_URL
