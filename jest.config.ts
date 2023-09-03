92b807fa3e25cf581790c682f2a8fd4b2e0d2beb
seanaxn1135 <seanaxn1135@gmail.com>
2023-09-04 01:49:07 +0800

    dependency: added playwright and simple test

--- jest.config.ts
+++ jest.config.ts
@@ -11,6 +11,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ['./jest.setup.js'], // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  modulePathIgnorePatterns: ['api_tests'],
 }
 
 module.exports = createJestConfig(customJestConfig)
