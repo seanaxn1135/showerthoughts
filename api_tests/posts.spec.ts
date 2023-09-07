9feab1601fd978a6c8b97cce2909be12dbccdb2d
seanaxn1135 <seanaxn1135@gmail.com>
2023-09-07 14:28:42 +0800

    api-test: create testing cases

--- api_tests/posts.spec.ts
+++ api_tests/posts.spec.ts
@@ -1,8 +1,19 @@
 import { test, expect } from '@playwright/test'
-import { createUser } from 'test-helpers/createUser'
+import { createUser, createUserRequest } from 'test-helpers/createUser'
 
-test('seeded', async ({ request }) => {
-  const user = await createUser()
-  const req = await request.get(`/api/users/${user.username}`)
-  expect(req.status()).toBe(200)
+test.describe.serial('#posts', async () => {
+  test.skip('should not create post if unauthrized', () => {})
+
+  let token: string = ''
+  test.beforeAll(async ({ request }) => {
+    const userRequest = createUserRequest()
+    await createUser({ ...userRequest })
+    const req = await request.post(`/api/token`, {
+      data: userRequest,
+    })
+    token = await req.json()
+  })
+  test.skip('should create post if authorized', () => {})
+  test.skip('should get all posts', () => {})
+  test.skip('should get post by title', () => {})
 })
--- test-helpers/createUser.ts
+++ test-helpers/createUser.ts
@@ -5,15 +5,20 @@ import { IUser } from '@/app/models/Users'
 import { faker } from '@faker-js/faker'
 dotenv.config()
 
-export async function createUser(): Promise<IUser> {
-  const username = `${faker.internet.userName()}_${Date.now().toString()}`
-  const password = faker.internet.password()
-  const userRequest = {
-    username: username,
-    password: password,
-  }
-
+export async function createUser(userRequest: {
+  username: string
+  password: string
+}): Promise<IUser> {
   const usersCollection = new UsersCollectionMongo()
   const userService = new UserService(usersCollection)
   return userService.createUser(userRequest)
+}
+
+export function createUserRequest(): { username: string; password: string } {
+  const username = `${faker.internet.userName()}_${Date.now().toString()}`
+  const password = faker.internet.password()
+  return {
+    username: username,
+    password: password,
+  }
 }
